dbb9b5ee939d8744d60d29794e8522a008e5ffc7
Walkline Wang <walkline@163.com>
2021-08-04 00:18:44 +0800

    修改内容以适合上传 PyPI

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 import ab
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as file:
+	long_description = file.read()
 
 setup(
 	name = 'ampy-batch-tool',
 	packages = ['ab'],
 	version = ab.__version__,
 	author = ab.__author__,
+	author_email = ab.__email__,
 	description = 'An adafruit-ampy batch tool',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
 	url = 'https://gitee.com/walkline/a-batch-tool',
-	license = 'MIT',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: Microsoft :: Windows',\
+		'Environment :: Console',
+	],
 	provides = ['ab'],
 	entry_points = {
 		'console_scripts': [
@@ -22,3 +32,10 @@ setup(
 		'pyminifier'
 	]
 )
+
+# python setup.py sdist upload
+
+# pip install -U twine wheel setuptools
+# python setup.py sdist bdist_wheel
+# twine check dist/*
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
