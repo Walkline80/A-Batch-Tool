58791dacb3df5abdb8dea89aa523b1c8b213b410
Walkline Wang <walkline@163.com>
2021-09-23 18:36:36 +0800

    再次修改 esptool 命令

--- ab/flash.py
+++ ab/flash.py
@@ -88,10 +88,10 @@ def run_esptool_shell():
 		print('no firmware file found')
 		exit()
 
-	firmware = choose_an_option('Firmware', firmware_list)
-	
-	earse_command = f'esptool.py {__PORT} {__BAUD} {__CHIP} erase_flash'
-	write_command = f'esptool.py {__PORT} {__BAUD} {__CHIP} {__BEFORE} {__AFTER} write_flash {__MODE} {__SIZE} {__FREQ} {addr} {firmware}'
+	firmware = list(reversed(choose_an_option('Firmware', firmware_list)))
+
+	earse_command = f'esptool {__PORT} {__BAUD} {__CHIP} erase_flash'
+	write_command = f'esptool {__PORT} {__BAUD} {__CHIP} {__BEFORE} {__AFTER} write_flash {__MODE} {__SIZE} {__FREQ} {addr} {firmware}'
 
 	try:
 		if os.system(earse_command) == 0:
